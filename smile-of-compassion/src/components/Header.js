7e05150258068c599ff3c4fd3f933f31b6ee2c72
telga <briann2305@gmail.com>
2024-10-13 19:27:28 -0600

    Updated Header

--- smile-of-compassion/src/components/Header.js
+++ smile-of-compassion/src/components/Header.js
@@ -1,19 +1,74 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem, useMediaQuery, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuItems = [
+    { text: 'Home', path: '/' },
+    { text: 'About', path: '/about' },
+    { text: 'Projects', path: '/projects' },
+    { text: 'Contact', path: '/contact' },
+    { text: 'Donate', path: '/donate' },
+  ];
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Smile of Compassion Projects
         </Typography>
-        <Button color="inherit" component={Link} to="/">Home</Button>
-        <Button color="inherit" component={Link} to="/about">About</Button>
-        <Button color="inherit" component={Link} to="/projects">Projects</Button>
-        <Button color="inherit" component={Link} to="/contact">Contact</Button>
-        <Button color="inherit" component={Link} to="/donate">Donate</Button>
+        {isMobile ? (
+          <>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {menuItems.map((item) => (
+                <MenuItem key={item.text} onClick={handleClose} component={Link} to={item.path}>
+                  {item.text}
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
+        ) : (
+          menuItems.map((item) => (
+            <Button key={item.text} color="inherit" component={Link} to={item.path}>
+              {item.text}
+            </Button>
+          ))
+        )}
       </Toolbar>
     </AppBar>
   );
