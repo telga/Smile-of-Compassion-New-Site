bf62790cbc752c9b8e37c3248f5f7387c6f4f850
telga <briann2305@gmail.com>
2024-10-14 01:05:21 -0600

    bw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Header from './components/Header';
 import Footer from './components/Footer';
